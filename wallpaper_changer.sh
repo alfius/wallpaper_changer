b89f0c546ec21cb5a146d9917bb803f5d653211e
Alfonso Cora <acora6@gmail.com>
2013-05-09 10:52:35 -0300

    Make the script work inside a cron job

--- wallpaper_changer.sh
+++ wallpaper_changer.sh
@@ -2,4 +2,9 @@
 wallpapers_dir=$HOME/Pictures/Wallpapers
 wallpaper="$(ls $wallpapers_dir/* | shuf -n1)"
 
+if [ -z "$DBUS_SESSION_BUS_ADDRESS" ] ; then
+  TMP=~/.dbus/session-bus
+  export $(grep -h DBUS_SESSION_BUS_ADDRESS= $TMP/$(ls -1t $TMP | head -n 1))
+fi
+
 gsettings set org.gnome.desktop.background picture-uri "file://$wallpaper"
